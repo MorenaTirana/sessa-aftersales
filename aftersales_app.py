df2c2fc6858be2f2eeafda8abab180ce711643a0
Morena Tirana <63554265+MorenaTirana@users.noreply.github.com>
2025-09-24 23:28:09 +0200

    aftersales_app.py

--- aftersales_app.py
+++ aftersales_app.py
@@ -108,7 +108,7 @@ section[data-testid="stSidebar"] input, section[data-testid="stSidebar"] textare
 }
 /* Abbassa solo il logo in sidebar */
 
-aside[aria-label="sidebar"] .sb-brand{ margin-top:30% !important; }
+aside[aria-label="sidebar"] .sb-brand{ margin-top:35% !important; }
 
 </style>
 """,
